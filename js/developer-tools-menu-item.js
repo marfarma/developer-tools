0e511f1defbe3ef1ccc1bf14c58e91a8da93f585
kjmeath <kjmeath@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-03-12 05:10:56 +0000

    updated click me alert
    git-svn-id: http://svn.wp-plugins.org/developer-tools/tags/1.0.4@359057 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/developer-tools-menu-item.js
+++ js/developer-tools-menu-item.js
@@ -2,6 +2,5 @@ if( typeof jQuery == 'function' )
   jQuery(function($){
     $('#toplevel_page_developer-tools a.toplevel_page_developer-tools').click( function(){
       document.cookie = 'developer_tools_current_menu_item=home';
-      alert('clicked');  
     });
   });
